9c47ebaff5061df734df838311d1b51e4e44ab6b
StoneDog02 <87345253+StoneDog02@users.noreply.github.com>
2021-11-16 20:42:43 -0700

    working animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
-import { Canvas } from "./components/Canvas";
-
+import {
+  Canvas,
+  Square,
+  Rect,
+  MotionBlur,
+  ClearCanvas,
+  Velocity
+} from "./components/Canvas";
+import { useCallback, useEffect, useState } from "react";
+const DEBRIS_VELOCITY = 2;
 export default function App() {
   return (
     <div className="App">
-      <Canvas />
+      <Canvas width="500" height="500">
+        <MotionBlur a={0.5} />
+        {/* <ClearCanvas /> */}
+        <Velocity d={{ x: DEBRIS_VELOCITY, y: DEBRIS_VELOCITY }}>
+          <Square x={0} y={0} width={50} color="aqua" />
+        </Velocity>
+        <Velocity d={{ x: -DEBRIS_VELOCITY, y: -DEBRIS_VELOCITY }}>
+          <Square x={450} y={450} width={50} color="rebeccapurple" />
+        </Velocity>
+        <Velocity d={{ x: DEBRIS_VELOCITY, y: -DEBRIS_VELOCITY }}>
+          <Square x={0} y={450} width={50} color="rebeccapurple" />
+        </Velocity>
+        <Velocity d={{ x: -DEBRIS_VELOCITY, y: DEBRIS_VELOCITY }}>
+          <Square x={450} y={0} width={50} color="aqua" />
+        </Velocity>
+        <Rect x={210} y={230} width={80} height={40} color="hotpink" />
+      </Canvas>
     </div>
   );
 }
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,3 +1,95 @@
-export function Canvas() {
-  return <div>Canvas</div>;
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+  useEffect,
+  useRef
+} from "react";
+
+const VelocityContext = createContext({});
+
+export function Velocity({ d, ...props }) {
+  return <VelocityContext.Provider value={d} {...props} />;
+}
+
+const CanvasContext = createContext({ canvasNode: null, ctx: null, frame: 0 });
+
+export function Canvas({ children, ...props }) {
+  const [canvasNode, setCanvasNode] = useState(null);
+  const canvasRef = useCallback((canvasNode) => {
+    setCanvasNode(canvasNode);
+  }, []);
+
+  const [frame, setFrame] = useState(0);
+
+  const draw = useCallback(
+    function draw() {
+      setFrame(frame + 1);
+    },
+    [frame]
+  );
+
+  useEffect(() => {
+    let raf = window.requestAnimationFrame(draw);
+
+    // TODO: Make this conditional
+
+    return () => {
+      window.cancelAnimationFrame(raf);
+    };
+  }, [draw]);
+
+  return (
+    <CanvasContext.Provider
+      value={{ canvasNode, ctx: canvasNode?.getContext("2d") || null, frame }}
+    >
+      <VelocityContext.Provider value={{}}>
+        <canvas ref={canvasRef} {...props} />
+        {!canvasNode ? null : children}
+      </VelocityContext.Provider>
+    </CanvasContext.Provider>
+  );
+}
+//Utils:
+export function MotionBlur({ r = 255, g = 255, b = 255, a = 0.3 }) {
+  const { ctx } = useContext(CanvasContext);
+  const { width, height } = ctx.canvas;
+  ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
+  ctx.fillRect(0, 0, width, height);
+  return null;
+}
+export function ClearCanvas(props) {
+  const { ctx } = useContext(CanvasContext);
+  const { width, height } = ctx.canvas;
+  ctx.clearRect(0, 0, width, height);
+  return null;
+}
+
+//Shapes:
+export function Square({ x, y, color, width }) {
+  const properties = useRef({ x, y, color, width });
+  const { ctx } = useContext(CanvasContext);
+  const { x: dx = 0, y: dy = 0 } = useContext(VelocityContext);
+  ctx.fillStyle = color;
+  // console.log("SquareProps", properties.current.x, properties.current.y);
+  ctx.fillRect(properties.current.x, properties.current.y, width, width);
+  useEffect(() => {
+    properties.current = {
+      x: properties.current.x + dx,
+      y: properties.current.y + dy
+    };
+  });
+  return null;
+}
+
+export function Rect({ x, y, color, width, height }) {
+  const { ctx, frame } = useContext(CanvasContext);
+  ctx.save();
+  ctx.translate(250, 250);
+  ctx.rotate((Math.PI / 180) * frame);
+  ctx.fillStyle = color;
+  ctx.fillRect(-40, -20, width, height);
+  ctx.restore();
+  return null;
 }
